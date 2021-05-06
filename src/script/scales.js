14e5d9275ffc33f1512183b85a65e2bded947495
hectarek <hectarek13@gmail.com>
2021-05-05 20:01:16 -0400

    added some scales

--- src/script/scales.js
+++ src/script/scales.js
@@ -78,9 +78,21 @@ export const scales = [
 		canLimitOctave: false,
 	},
 	{
-		name: "Bebop Scale",
+		name: "Major Bebop",
 		notesInC: [],
-		pattern: [0,2,4,6,8,9,10,12],
+		pattern: [0,2,4,5,7,8,9,11,12],
+		canLimitOctave: false,
+	},
+	{
+		name: "Dominant Bebop",
+		notesInC: [],
+		pattern: [0,2,4,5,7,9,10,11,12],
+		canLimitOctave: false,
+	},
+	{
+		name: "Altered Dominant Scale",
+		notesInC: [],
+		pattern: [0,1,3,4,6,8,10,12],
 		canLimitOctave: false,
 	},
 	{
